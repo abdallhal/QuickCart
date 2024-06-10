102a55d7db09ebdc9ab7ccfe175ff26481452dab
Abdallah Ali <Abdallah.Mohamed@sa.solutions>
2024-06-10 08:53:09 +0300

    add migrations

---
+++ QuickCart.DataAccess/Migrations/20240610043323_add-sub-category-tbl.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuickCart.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addsubcategorytbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SubCategory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubCategory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubCategory_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubCategory_CategoryId",
+                table: "SubCategory",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SubCategory");
+        }
+    }
+}
---
+++ QuickCart.DataAccess/Migrations/20240610043447_add-product-tbl.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuickCart.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addproducttbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubCategory_Categories_CategoryId",
+                table: "SubCategory");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SubCategory",
+                table: "SubCategory");
+
+            migrationBuilder.RenameTable(
+                name: "SubCategory",
+                newName: "SubCategories");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SubCategory_CategoryId",
+                table: "SubCategories",
+                newName: "IX_SubCategories_CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SubCategories",
+                table: "SubCategories",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SubCategoryId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_SubCategories_SubCategoryId",
+                        column: x => x.SubCategoryId,
+                        principalTable: "SubCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_SubCategoryId",
+                table: "Products",
+                column: "SubCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubCategories_Categories_CategoryId",
+                table: "SubCategories",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubCategories_Categories_CategoryId",
+                table: "SubCategories");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SubCategories",
+                table: "SubCategories");
+
+            migrationBuilder.RenameTable(
+                name: "SubCategories",
+                newName: "SubCategory");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SubCategories_CategoryId",
+                table: "SubCategory",
+                newName: "IX_SubCategory_CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SubCategory",
+                table: "SubCategory",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubCategory_Categories_CategoryId",
+                table: "SubCategory",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
