82cde088ef8da3a31f2016b3f2315cce50808e34
Abdallah Ali <Abdallah.Mohamed@sa.solutions>
2024-06-10 07:31:49 +0300

    add sub category dto

---
+++ QuickCart.Domain/DTO/SubCategory/CreateSubCategoryDTO.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace QuickCart.Domain.DTO
+{
+    public class CreateSubCategoryDTO
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
