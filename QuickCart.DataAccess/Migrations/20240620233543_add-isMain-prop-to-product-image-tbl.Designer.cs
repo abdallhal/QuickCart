13d456aa32681f2734897402a66ddefd92169047
Abdallah Ali <Abdallah.Mohamed@sa.solutions>
2024-06-21 02:48:52 +0300

    add migrations file

--- QuickCart.DataAccess/Migrations/20240620233543_add-isMain-prop-to-product-image-tbl.Designer.cs
+++ QuickCart.DataAccess/Migrations/20240620233543_add-isMain-prop-to-product-image-tbl.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuickCart.DataAccess.Models;
 
@@ -11,9 +12,11 @@ using QuickCart.DataAccess.Models;
 namespace QuickCart.DataAccess.Migrations
 {
     [DbContext(typeof(QuickCartDbContext))]
-    partial class QuickCartDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240620233543_add-isMain-prop-to-product-image-tbl")]
+    partial class addisMainproptoproductimagetbl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
