b9ad98095c3a289c954065fd49d1070a0dc963b4
Abdallah Ali <Abdallah.Mohamed@sa.solutions>
2024-07-08 18:57:04 +0300

    Add category and subcategory endpoints

---
+++ QuickCart.Web/Areas/Admin/Api/CategoriesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuickCart.Services;
+
+namespace QuickCart.Web.Areas.Admin.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+
+        private readonly ICategoryService _service;
+        public CategoriesController(ICategoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("GetCategories")]
+        public async Task<IActionResult> GetCategories()
+        {
+
+            try
+            {
+                var response = await _service.GetAllSelectListItemAsync();
+
+                return Ok(response);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+   
+
+        }
+    }
+}
---
+++ QuickCart.Web/Areas/Admin/Api/SubCategoriesController.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using QuickCart.Services;
+
+namespace QuickCart.Web.Areas.Admin.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubCategoriesController : ControllerBase
+    {
+        private readonly ISubCategoryService _service;
+        public SubCategoriesController(ISubCategoryService service)
+        {
+            _service = service; 
+            
+        }
+
+
+        [HttpGet("GetSubCategories/{categoryId}")]
+        public async Task<IActionResult> GetSubCategories(int categoryId)
+        {
+
+            var response = await _service.GetAllSelectListItemAsync(categoryId);
+
+            return Ok(response);
+
+        }
+    }
+}
